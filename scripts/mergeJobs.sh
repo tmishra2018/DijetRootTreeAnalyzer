32df636bef4d3fd5a52d536819a7e29328a7da4c
Francesco Santanastasio <francesco.santanastasio@cern.ch>
2015-07-30 15:17:32 +0200

    create merged directory by default

--- scripts/mergeJobs.sh
+++ scripts/mergeJobs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-DIR='/pnfs/roma1.infn.it/data/cms/store/user/roma-group1/Dijet/reducedTrees/mc/output_Spring15_v2_JESminus5percent/'
+DIR='/pnfs/roma1.infn.it/data/cms/store/user/santanas/Dijet/reducedTrees/data/santanas__40pb-1_30_07_2015/'
 #DIR='/pnfs/roma1.infn.it/data/cms/store/user/roma-group1/Dijet/reducedTrees/data/25July2015-CertJson-251586-251883_JEC-Summer15_50nsV2_501dfb2/'
+mkdir $DIR/merged/
 ENDPOINT="dcap://cmsrm-se01.roma1.infn.it/"
 for i in `ls $DIR/*_reduced_skim.root|sed 's/[0-9]*_reduced_skim.root//'|sort -u`
 do
