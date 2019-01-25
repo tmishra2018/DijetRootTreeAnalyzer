32395eba319c431e9cef726ad3effc0ce6daaaa8
lucastorterotot <lucas.torterotot@gmail.com>
2019-01-25 10:35:02 +0100

    Updated scripts for debug.
    Conflicts:
    	Run_Analyser.py
    	Run_Analyser_short.py
    	Submit_to_HTcondor.sub

--- Run_Analyser_short.py
+++ Run_Analyser_short.py
@@ -3,7 +3,8 @@ import argparse, os, tempfile, shutil, sys,math,pickle,itertools
 from subprocess import call, PIPE, STDOUT, Popen
 import argparse
 import datetime
-parser = argparse.ArgumentParser(description='Submit jot to batch')
+import os
+parser = argparse.ArgumentParser(description='Submit job to batch')
 parser.add_argument('--config_file',type=str,dest="config_file",default=1, help=" config_file of the era")
 parser.add_argument('--outputname',type=str,dest="outputname",default=1, help="Run era")
 parser.add_argument('--list',type=str,dest="list",default=1, help="list of file to run")
@@ -15,7 +16,7 @@ listtorun = args.list
 dirlist = args.dirlist
 outputdir = "/eos/cms/store/group/phys_smp/hlattaud/Output_Batch/"#"/eos/user/h/hlattaud/GammaJet/2016/Output_lsfBatch/"
 cmd4="./main "+dirlist+listtorun+" "+config_file+" dijets/events "+outputdir+outputname+" "+outputdir+outputname
-cmd1="cd /afs/cern.ch/work/h/hlattaud/private/CMSSW_8_0_25/src/CMSDIJET/responsecomputing/DijetRootTreeAnalyzer/"
+cmd1="cd /afs/cern.ch/work/l/ltortero/JEC-task/CMSSW_8_0_31/src/CMSDIJET/DijetRootTreeAnalyzer/"
 cmd2="export SCRAM_ARCH=slc6_amd64_gcc530"
 cmd3="eval `scramv1 runtime -sh`"
 cmd5="export X509_USER_PROXY=/afs/cern.ch/user/h/hlattaud/.globus/gridproxy.cert" 
--- command_HTcondor.sh
+++ command_HTcondor.sh
@@ -1,8 +1,9 @@
 cd $CMSSW_BASE/src/CMSDIJET/DijetRootTreeAnalyzer
 mkdir -p /afs/cern.ch/work/${USER:0:1}/$USER/private/HT_Condor_output/DijetRootTreeAnalyzer/output_txtfile/
 mkdir -p log
-voms-proxy-init --voms cms --valid 100:00 -out /afs/cern.ch/user/${USER:0:1}/$USER/.globus/gridproxy.cert
-condor_submit output_dir=/afs/cern.ch/work/${USER:0:1}/$USER/private/HT_Condor_output/DijetRootTreeAnalyzer/output_txtfile/ config_File=config/cutFile_mainGammaplusJetSelection_Newsmearing.txt output_name=TEST_condor name_list=test_prefix dir_list=dir_list_test/ Njob=1 Submit_to_HTcondor.sub
+voms-proxy-init --voms cms --valid 100:00 -out ~/.globus/gridproxy.cert
+
+condor_submit config_File=config/cutFile_mainGammaplusJetSelection_Newsmearing.txt output_name=TEST_condor name_list=test_prefix dir_list=dir_list_test/ Njob=1  Submit_to_HTcondor.sub
 # name_list = prefixe de la liste, voir avec la commande split -l10 -d long_list.txt
 # dir_list = repertoire contenant les listes
 # Njobs = nb de listes
