b4912878f07fc824296677e9e79d41cabc8c11b0
lucastorterotot <lucas.torterotot@gmail.com>
2019-02-18 11:44:32 +0100

    Updated jed ID for 2018.

--- src/analysisClass_GammaplusJet_New_methods.C
+++ src/analysisClass_GammaplusJet_New_methods.C
@@ -74,44 +74,27 @@ return isValid;
 }*/
 
 // Jet selection Cut ID from this twiki : https://twiki.cern.ch/twiki/bin/view/CMS/JetID13TeVRun2016
-bool isNewonOldValidJetTight(const float& Eta_ak4, const float& nhf, const float& neMult, const float& chMult, const float& nemf, const float& muF, const float& chf, const float& cemF, const bool& isoldvalid, const bool& isDATA, const bool& hasgenjet, const bool& isFirst)
+bool isNewonOldValidJetTight(const float& Eta_ak4, const float& nhf, const float& neMult, const float& chMult, const float& nemf, const float& muf, const float& chf, const float& cemf, const bool& isoldvalid, const bool& isDATA, const bool& hasgenjet, const bool& isFirst)
 {
-   
-    int idL = -999 ;
+  int idL = -999 ;
     
-  
-  if(fabs(Eta_ak4) > 3.0){
-    idL = ( neMult > 10 && nhf > 0.02 && nemf < 0.9 );
-  
-  }
-    
-    if(fabs(Eta_ak4) > 2.7  && fabs(Eta_ak4) <= 3.0)
+  if(fabs(Eta_ak4) > 3.0)
     {
-       idL = ( nemf < 0.99 && neMult>2)  ;
-      
-      
+      idL = ( nemf<0.90 && neMult>10 ) ;
+    }else{
+    if(fabs(Eta_ak4) > 2.7)
+      {
+        idL = ( nemf>0.01 && nhf<0.98 && neMult > 2 );
+      }else{
+      if(fabs(Eta_ak4)>2.6)
+        {
+          idL = ( nemf>0.01 && nhf<0.98 && neMult > 2 );
+        }else{
+        idL = ( nemf>0.01 && nhf<0.98 && neMult > 2 ) ;
       }
-      
-      if(fabs(Eta_ak4) >= 2.4  && fabs(Eta_ak4) <= 2.7){
-        
-        idL = (nhf < 0.9 && nemf < 0.9  && neMult >1 && muF < 0.8);
-        
-        
-        
-      }
-      
-       if( fabs(Eta_ak4) < 2.4 ){
-  
-  
-          idL = (nhf < 0.9 && nemf < 0.9  && neMult >1 && muF < 0.8 && chMult > 0 && chf > 0 && cemF < 0.8) ;
-      
-      }
-     
-    
- return idL;
-
-    
-    
+    }
+  }
+  return idL;
 }
 
 
