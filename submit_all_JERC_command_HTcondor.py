c4a241386b4a13df77e8b57ca5b227ab76a3ad8f
lucastorterotot <lucas.torterotot@gmail.com>
2020-03-24 15:46:23 +0100

    Update submit_all_JERC_command_HTcondor.py from 2018 to 2017UL

--- submit_all_JERC_command_HTcondor.py
+++ submit_all_JERC_command_HTcondor.py
@@ -6,11 +6,16 @@ configs={}
 cutfiles={}
 
 samples = {
-    'A' : 'Run2018A-17Sep2018-v2',
-    'B' : 'Run2018B-17Sep2018-v1',
-    'C' : 'Run2018C-17Sep2018-v1',
-    'D' : 'Run2018D-PromptReco-v2',
-    'MC': 'GJet_Pt-15To6000_RunIIAutumn18MiniAOD-102X',
+    'B' : 'Run2017B-09Aug2019_UL2017-v1',
+    'C' : 'Run2017C-09Aug2019_UL2017-v1',
+    'D' : 'Run2017D-09Aug2019_UL2017-v1',
+    'E' : 'Run2017E-09Aug2019_UL2017-v1',
+    'F' : 'Run2017F-09Aug2019_UL2017-v1',
+    'MC1': 'GJets_HT-40To100_RunIISummer19MiniAOD-106X',
+    'MC2': 'GJets_HT-100To200_RunIISummer19MiniAOD-106X',
+    'MC3': 'GJets_HT-200To400_RunIISummer19MiniAOD-106X',
+    'MC4': 'GJets_HT-400To600_RunIISummer19MiniAOD-106X',
+    'MC5': 'GJets_HT-600ToInf_RunIISummer19MiniAOD-106X',
 }
 
 tags = {
@@ -26,16 +31,16 @@ runs = samples.keys()
 for run in runs:
     for JERC in JERCs:
         key = '_'.join([run,JERC])
-        if run == 'MC':
-            cutfiles[key] = cutfiles['_'.join(['A',JERC])]
+        if 'MC' in run:
+            cutfiles[key] = cutfiles['_'.join(['B',JERC])]
         else:
-            cutfiles[key] = 'config/cutFile_Run2018{run}_{JERC}.txt'.format(
+            cutfiles[key] = 'config/cutFile_Run2017UL{run}_{JERC}.txt'.format(
             run=run,
             JERC=JERC
         )
         configs[key] = '{cutfile} {dirlist} {tag}'.format(
             cutfile = cutfiles[key],
-            dirlist = 'lists_2018/'+samples[run],
+            dirlist = 'lists_2017UL/'+samples[run],
             tag = tags[JERC]
         )
         command += ' '+configs[key]
